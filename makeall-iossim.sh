eea79129e5b7d4b8aad1c5819be6d65af924e3c1
Cameron Webb <cwebb02@gmail.com>
2014-04-16 13:40:12 -0500

    just not going to runhooks in travis. will need to do on local machine

--- makeall-iossim.sh
+++ makeall-iossim.sh
@@ -39,8 +39,8 @@ function upgradeSVN()
 
 function buildios() {
 echo "-- building webrtc ios"
-pushd trunk/
-gclient runhooks && ninja -C out_ios/Debug-iphoneos
+pushd trunk
+ninja -C out_ios/Debug-iphoneos
 popd
 echo "-- webrtc has been sucessfully built"
 }
