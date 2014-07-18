6206175538ab5662bfb416d59c9851dcb0cc0bb0
Cameron Webb <cwebb02@gmail.com>
2014-07-18 10:26:58 -0500

    cleaning up build scripts

--- combine_libs.sh
+++ combine_libs.sh
@@ -1,9 +1,19 @@
 _dfiles="trunk/out_ios/Release-iphoneos/*.a"
 
+mkdir $"built_libs/"
+mkdir $"built_libs/device/"
+mkdir $"built_libs/simulator/"
+
 for f in $_dfiles
 do
 	IFS='/' read -a array <<< "$f"
 	pwd
-	lipo -create $"trunk/out_ios/Release-iphoneos/${array[3]}" "trunk/out_sim/Release-iphonesimulator/${array[3]}" -output "built_libs/${array[3]}"
+	# (lipo -create $"trunk/out_ios/Release-iphoneos/${array[3]}" $"trunk/out_sim/Release-iphonesimulator/${array[3]}" -output $"built_libs/universal_${array[3]}")
+	
+	cp $"trunk/out_ios/Release-iphoneos/${array[3]}" $"built_libs/device/device_${array[3]}"
+	cp $"trunk/out_ios/Release-iphonesimulator/${array[3]}" $"built_libs/simulator/simulator_${array[3]}"
 	
 done
+
+#Now go get headers
+
